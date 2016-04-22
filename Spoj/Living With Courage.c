9a6eea424edad750647f9aa7463e63dbef5c3f26
HarshGrandeur <hrshgpt73@gmail.com>
2016-04-22 13:40:39 +0530

    Update Living With Courage.c
    hey man there seems to be some problem with your code. I have uploaded the correct one.

--- Spoj/Living With Courage.c
+++ Spoj/Living With Courage.c
@@ -1,103 +1,140 @@
-//http://www.spoj.com/problems/COURAGE/
-//Author Ashish Kedia, NITK Surathkal
-//@ashish1294
+//Author Harsh Gupta
 #include<stdio.h>
+
+#include<stdlib.h>
 #include<limits.h>
 #define min(a,b) (a<b?a:b)
-struct node{
-    int s,m;
-};
-struct node tree[1000000];
-long long int app[100001];
-inline struct node init(int si,int ss,int se)
-{
-    if(ss==se)
+    struct node
     {
-        tree[si].s=tree[si].m=app[ss];
-        return tree[si];
+		long long int s,m;
+    };
+
+ struct node tree[1000000];
+ long long int app[100001];
+
+    long long int getMid(long long int s,long long int e) {
+	return s + ((e - s) / 2);
     }
-    int next = si+si,mid = ss + ((se-ss)/2);
-    struct node aa = init(next+1,ss,mid),bb = init(next+2,mid+1,se);
-    tree[si].s = aa.s + bb.s;
-    tree[si].m = min(aa.m,bb.m);
-    return tree[si];
-}
-inline struct node update(int si,int ss,int se,int val,int in)
-{
-    if(ss==se)
-    {
-        tree[si].s +=val;
-        tree[si].m = tree[si].s;
-        return tree[si];
-    }
-    int next = si+si,mid = ss + ((se-ss)/2);
-    struct node aa,bb;
-    if(in<=mid)
-    {
-        aa = update(next+1,ss,mid,val,in);
-        bb = tree[next+2];
-    }
-    else
-    {
-        aa = tree[next+1];
-        bb = update(next+2,mid+1,se,val,in);
-    }
-    tree[si].s = aa.s + bb.s;
-    tree[si].m = min(aa.m,bb.m);
-    return tree[si];
-}
-inline struct node query(int si,int ss,int se,int l,int r)
-{
-    if(l<=ss && r>=se) return tree[si];
-    if(l>se || r<ss)
-    {
-        struct node aa;aa.s=0;aa.m = INT_MAX;
-        return aa;
-    }
-    int next = 2LL*si,mid = ss + ((se-ss)/2);
-    struct node aa = query(next+1,ss,mid,l,r),bb = query(next+2,mid+1,se,l,r),cc;
-    cc.s = aa.s+bb.s;
-    cc.m = min(aa.m,bb.m);
-    return cc;
-}
-inline int inp()
-{
-    register int noRead=0;
-    //register char p=getchar_unlocked();
-    register char p=getc(stdin);
-    //for(;p<48 || p>57;p=getchar_unlocked());
-    for(;p<48 || p>57;p=getc(stdin));
-    //while(p>47 && p<58){ noRead = (noRead << 3) + (noRead << 1) + (p - '0');p=getchar_unlocked();}
-    while(p>47 && p<58){ noRead = (noRead << 3) + (noRead << 1) + (p - '0');p=getc(stdin);}
-    return noRead;
-};
-inline char inpc()
-{
-    //register char p=getchar_unlocked();
-    register char p=getc(stdin);
-    //for(;p<65 || p>90;p=getchar_unlocked());
-    for(;p<65 || p>90;p=getc(stdin));
-    return p;
-};
-int main()
-{
-    int n,i,p,x,y;
-    char event;
-    n=inp();
-    for(i=0;i<n;i++) app[i]=inp();
-    init(0,0,n-1);
-    p=inp();
-    while(p--)
-    {
-        event=inpc();x=inp();y=inp();
-        if (event=='E') update(0,0,n-1,-x,y);
-        else if(event=='G') update(0,0,n-1,x,y);
-        else if(event=='C')
-        {
-            struct node ans = query(0,0,n-1,x,y);
-            long long int as = ans.s - ans.m;
-            printf("%lld\n",as);
-        }
-    }
-    return 0;
-}
+
+	struct node init(long long int ss,long long int se,long long int si)
+	{
+		struct node aa,bb;
+		long long int mid;
+		if(ss==se)
+		{
+			tree[si].s=tree[si].m=app[ss];
+	    return tree[si];
+		}
+		mid=getMid(ss,se);
+		aa=init(ss,mid,2*si+1);
+		bb=init(mid+1,se,2*si+2);
+		tree[si].s=aa.s+bb.s;
+		tree[si].m=min(aa.m,bb.m);
+		return tree[si];
+	}
+	
+	struct node update(long long int ss,long long int se,long long int si,long long int diff,long long int i)
+	{
+				
+		long long int mid;
+		struct node aa,bb;
+			
+		
+		//printf("Value of si %lld and si %lld\n",tree[si].s,si);
+		if(ss==se)
+		{
+			
+			//printf("MIN, SUM ini value of  %lld %lld\n ",tree[si].m,tree[si].s);
+			tree[si].s=tree[si].s + diff;
+			tree[si].m=tree[si].s;
+			//printf("MIN final value of  %lld \n ",tree[si].m);
+			//printf("MIN, SUM final value of  %lld %lld\n ",tree[si].m,tree[si].s);
+			return tree[si];
+			
+		}
+		
+		mid=getMid(ss,se);
+		if(i<=mid)
+		{
+			aa=update(ss,mid,2*si+1,diff,i);
+			bb=tree[2*si+2];
+		}
+		else{
+			aa=tree[2*si+1];
+			bb=update(mid+1,se,2*si+2,diff,i);
+			
+		}
+		//tree[si].s=aa.s+bb.s;
+		tree[si].s=aa.s+bb.s;
+		tree[si].m=min(aa.m,bb.m);
+		return tree[si];
+				
+	}
+
+	struct node query(long long int ss,long long int se,long long int qs,long long int qe,long long int si)
+	{
+		long long int mid;
+		struct node aa,bb,cc;
+		if (qs <= ss && qe >= se)
+	    return tree[si];
+
+		if(qs>se || qe<ss)
+		{
+
+			aa.s=0;
+			aa.m = 9999999999999;
+			return aa;
+
+		}
+		mid=getMid(ss,se);
+		aa=query(ss,mid,qs,qe,2*si+1);
+		bb=query(mid+1,se,qs,qe,2*si+2);
+		cc.s=aa.s+bb.s;
+		cc.m=min(aa.m,bb.m);
+		return cc;
+	}
+
+
+
+
+	int main(void)
+	{
+
+		long long int n,p,x,y,i,s;	
+		struct node hh;
+	    char in[10];
+		scanf("%lld",&n);
+		for(i=0;i<n;i++)
+			scanf("%lld",&app[i]);
+		for(i=0;i<1000000;i++)
+			tree[i].m=INT_MAX;
+		
+		//printf("%lld",tree[25].m);
+		init(0,n-1,0);
+		scanf("%lld",&p);
+		while(p>0)
+		{
+
+			scanf("%s",in);
+			scanf("%lld",&x);
+			scanf("%lld",&y);
+			if(in[0]=='E')
+				update(0,n-1,0,-x,y);
+
+			if(in[0]=='G')
+				update(0,n-1,0,x,y);
+
+			if(in[0]=='C')
+			{
+				hh=query(0,n-1,x,y,0);
+				s=hh.s-hh.m;
+				//printf("min %lld\n",hh.m);
+				printf("%lld\n",s);
+			}
+			p--;
+
+		}
+
+
+	}
+
